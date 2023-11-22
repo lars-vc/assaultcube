17f742cffb8bcbfbeaac37f1a9c72aea109a12a8
Lars Van Cauter <larsvancauter@gmail.com>
2023-11-22 09:47:16 +0100

    protector

--- src/lars.h
+++ src/lars.h
@@ -21,11 +21,40 @@
 ///////////////////////////////////////////
 // simple xor
 ///////////////////////////////////////////
-inline int val_mask = rand();
+// inline int val_mask = rand();
+//
+// inline int get_val_lars(int val) { return val ^ val_mask; }
+// inline void set_val_lars(int &val, int set) { val = set ^ val_mask; }
+//
+// inline void add_val_lars(int &val, int add) {
+//     set_val_lars(val, get_val_lars(val) + add);
+// }
 
-inline int get_val_lars(int val) { return val ^ val_mask; }
-inline void set_val_lars(int &val, int set) { val = set ^ val_mask; }
+///////////////////////////////////////////
+// protector
+///////////////////////////////////////////
 
-inline void add_val_lars(int &val, int add) {
-    set_val_lars(val, get_val_lars(val) + add);
-}
+class Protector {
+  public:
+    std::vector<int> masks;
+    Protector() {}
+};
+
+class ProtectedInt {
+  private:
+    Protector *protector;
+    int obfuscated_val;
+    int id;
+    int deobfuscate() { return protector->masks[id] ^ obfuscated_val; }
+    int obfuscate(int val) { return protector->masks[id] ^ val; }
+
+  public:
+    ProtectedInt operator=(int val) {
+        obfuscated_val = obfuscate(val);
+        return *this;
+    }
+    ProtectedInt operator=(ProtectedInt val) {
+        obfuscated_val = val.obfuscated_val;
+        return *this;
+    }
+};
